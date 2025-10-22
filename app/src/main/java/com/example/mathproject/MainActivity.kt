9b8c775f2781f030e24732fd5df86eea26c2fb3a
Felipe Santiago <felipe.souza@texs.com.br>
2025-10-22 13:41:55 -0300

    Feat quiz page

--- app/src/main/java/com/example/mathproject/MainActivity.kt
+++ app/src/main/java/com/example/mathproject/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.mathproject
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.activity.enableEdgeToEdge
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,12 +10,14 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_difficulty)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.difficulty)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_difficulty)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.difficulty)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+        startActivity(Intent(this, QuizActivity::class.java))
+        finish()
     }
 }
