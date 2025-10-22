b8c9364a851eca71ee06df51f148b48b3f9b6d07
Felipe Santiago <felipe.souza@texs.com.br>
2025-10-22 03:53:16 -0300

    Feat Difficulty and Content Pages

--- app/src/main/java/com/example/mathproject/MainActivity.kt
+++ app/src/main/java/com/example/mathproject/MainActivity.kt
@@ -10,8 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_series)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.series)) { v, insets ->
+        setContentView(R.layout.activity_difficulty)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.difficulty)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
