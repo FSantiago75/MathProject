2af15c9524832ee64ac8d831f52da1eb511e504c
hyhash <hy.hyhash@gmail.com>
2025-10-25 18:24:10 -0300

    navegacao funcionando e variaveis para definir o quiz prontas

--- app/src/main/java/com/example/mathproject/ContentActivity.kt
+++ app/src/main/java/com/example/mathproject/ContentActivity.kt
@@ -1,11 +1,68 @@
 package com.example.mathproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ContentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content)
+
+        val btnContent1: Button = findViewById(R.id.btnContent1)
+        val btnContent2: Button = findViewById(R.id.btnContent2)
+        val btnContent3: Button = findViewById(R.id.btnContent3)
+        val btnContent4: Button = findViewById(R.id.btnContent4)
+        val btnContent5: Button = findViewById(R.id.btnContent5)
+        val btnBack: Button = findViewById(R.id.btnBack)
+
+        val Serie = intent.getStringExtra("EXTRA_SERIE")
+        val logSerie = Serie ?: "NÃO DEFINIDO"
+
+        btnContent1.setOnClickListener {
+            val difficulty = Intent(this, DifficultyActivity::class.java).apply {
+                putExtra("EXTRA_SERIE", logSerie)
+                putExtra("EXTRA_CONTEUDO", btnContent1.text)
+            }
+            startActivity(difficulty)
+        }
+
+        btnContent2.setOnClickListener {
+            val difficulty = Intent(this, DifficultyActivity::class.java).apply {
+                putExtra("EXTRA_SERIE", logSerie)
+                putExtra("EXTRA_CONTEUDO", btnContent2.text)
+            }
+            startActivity(difficulty)
+        }
+
+        btnContent3.setOnClickListener {
+            val difficulty = Intent(this, DifficultyActivity::class.java).apply {
+                putExtra("EXTRA_SERIE", logSerie)
+                putExtra("EXTRA_CONTEUDO", btnContent3.text)
+            }
+            startActivity(difficulty)
+        }
+
+        btnContent4.setOnClickListener {
+            val difficulty = Intent(this, DifficultyActivity::class.java).apply {
+                putExtra("EXTRA_SERIE", logSerie)
+                putExtra("EXTRA_CONTEUDO", btnContent4.text)
+            }
+            startActivity(difficulty)
+        }
+
+        btnContent5.setOnClickListener {
+            val difficulty = Intent(this, DifficultyActivity::class.java).apply {
+                putExtra("EXTRA_SERIE", logSerie)
+                putExtra("EXTRA_CONTEUDO", btnContent5.text)
+            }
+            startActivity(difficulty)
+        }
+
+        btnBack.setOnClickListener {
+            val series = Intent(this, SeriesActivity::class.java)
+            startActivity(series)
+        }
     }
 }
--- app/src/main/java/com/example/mathproject/DifficultyActivity.kt
+++ app/src/main/java/com/example/mathproject/DifficultyActivity.kt
@@ -1,11 +1,77 @@
 package com.example.mathproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class DifficultyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_difficulty)
+
+        val btnEasy: Button = findViewById(R.id.btnEasy)
+        val btnMedium: Button = findViewById(R.id.btnMedium)
+        val btnHard: Button = findViewById(R.id.btnHard)
+        val btnBack: Button = findViewById(R.id.btnBack)
+
+        val Serie = intent.getStringExtra("EXTRA_SERIE")
+        val Conteudo = intent.getStringExtra("EXTRA_CONTEUDO")
+        val logSerie = Serie ?: "NÃO DEFINIDO"
+        val logConteudo = Conteudo ?: "NÃO DEFINIDO"
+
+        btnEasy.setOnClickListener {
+
+            val quiz = Intent(this, QuizActivity::class.java).apply {
+                putExtra("EXTRA_SERIE", logSerie)
+                putExtra("EXTRA_CONTEUDO", logConteudo)
+                putExtra("EXTRA_DIFICULDADE", btnEasy.text)
+            }
+
+            // (Opcional) Adiciona dados para a próxima tela, como o tema escolhido
+            //intent.putExtra("EXTRA_TEMA", "Matemática Básica")
+
+            // Inicia a nova Activity
+            startActivity(quiz)
+        }
+
+        btnMedium.setOnClickListener {
+
+            val quiz = Intent(this, QuizActivity::class.java).apply {
+                putExtra("EXTRA_SERIE", logSerie)
+                putExtra("EXTRA_CONTEUDO", logConteudo)
+                putExtra("EXTRA_DIFICULDADE", btnMedium.text)
+            }
+
+            // (Opcional) Adiciona dados para a próxima tela, como o tema escolhido
+            //intent.putExtra("EXTRA_TEMA", "Matemática Básica")
+
+            // Inicia a nova Activity
+            startActivity(quiz)
+        }
+
+        btnHard.setOnClickListener {
+
+            val quiz = Intent(this, QuizActivity::class.java).apply {
+                putExtra("EXTRA_SERIE", logSerie)
+                putExtra("EXTRA_CONTEUDO", logConteudo)
+                putExtra("EXTRA_DIFICULDADE", btnHard.text)
+            }
+
+            // (Opcional) Adiciona dados para a próxima tela, como o tema escolhido
+            //intent.putExtra("EXTRA_TEMA", "Matemática Básica")
+
+            // Inicia a nova Activity
+            startActivity(quiz)
+        }
+
+        btnBack.setOnClickListener {
+            val content = Intent(this, ContentActivity::class.java).apply{
+                putExtra("EXTRA_SERIE", logSerie)
+            }
+            startActivity(content)
+        }
     }
+
+
 }
